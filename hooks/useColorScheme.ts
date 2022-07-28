dc1eb4212817b372e6a1baa05c0ef9a18a14ce24
Cloud Soh <cloudsoh03021@gmail.com>
2022-07-28 15:08:42 +0800

    12 - Fix navbar color

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -7,5 +7,8 @@ import {
 // type suggests that it can be null. This will not happen in practice, so this
 // makes it a bit easier to work with.
 export default function useColorScheme(): NonNullable<ColorSchemeName> {
+  // @todo dark theme
+  return "light";
+  // eslint-disable-next-line
   return _useColorScheme() as NonNullable<ColorSchemeName>;
 }
